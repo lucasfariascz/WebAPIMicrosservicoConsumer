835cabb2f1455f59edb0cc5568fb3a06dbfab886
lucasfariascz <lucasfariasdacruz@gmail.com>
2023-07-26 16:30:37 -0300

    #estrutura do projeto

--- WebAPIMicrosservicoConsumer/Services/Queue/QueueConsumerService.cs
+++ WebAPIMicrosservicoConsumer/Services/Queue/QueueConsumerService.cs
@@ -1,16 +1,14 @@
 ﻿using CosmosDBExemple.Data;
 using Microsoft.Azure.ServiceBus;
-using System.Text;
 using System.Text.Json;
 using WebAPIMicrosservico.Config.ServiceBus;
-using WebAPIMicrosservico.Features.User.Domain.Models;
 using WebAPIMicrosservicoConsumer.Features.Services.Models;
 
 namespace WebAPIMicrosservicoConsumer.Features.Services
 {
     public class QueueConsumerService : IHostedService
     {
-        private readonly NoSQLDatabase<UserViewModel> _noSQLDataBase;
+        private readonly NoSQLDatabase<UserViewModel> noSQLDataBase;
         public string container = "WebAPIMicroConsumer";
         static IQueueClient queueClient;
         private static readonly string QueueName = AppSettings.QueueName;
@@ -18,20 +16,20 @@ namespace WebAPIMicrosservicoConsumer.Features.Services
 
         public QueueConsumerService()
         {
-            _noSQLDataBase = new();
+            noSQLDataBase = new();
             queueClient = new QueueClient(AzureServiceBus, QueueName);
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("############## Starting Consumer - Queue ####################");
+            // Starting Consumer - Queue
             ProcessMessageHandler();
             return Task.CompletedTask;
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("############## Stopping Consumer - Queue ####################");
+            // Stopping Consumer - Queue
             await queueClient.CloseAsync();
             await Task.CompletedTask;
         }
@@ -49,23 +47,16 @@ namespace WebAPIMicrosservicoConsumer.Features.Services
 
         private async Task ProcessMessagesAsync(Message message, CancellationToken token)
         {
-            Console.WriteLine("### Processing Message - Queue ###");
-            Console.WriteLine($"{DateTime.Now}");
-            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
-            UserViewModel _userViewModel = JsonSerializer.Deserialize<UserViewModel>(message.Body);
-            await _noSQLDataBase.Add(container, _userViewModel, _userViewModel.Id.ToString());
+            // Processing Message - Queue
+            UserViewModel userViewModel = JsonSerializer.Deserialize<UserViewModel>(message.Body);
+            await this.noSQLDataBase.Add(container, userViewModel, userViewModel.Id.ToString());
 
             await queueClient.CompleteAsync(message.SystemProperties.LockToken);
         }
 
         private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
         {
-            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
-            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
-            Console.WriteLine("Exception context for troubleshooting:");
-            Console.WriteLine($"- Endpoint: {context.Endpoint}");
-            Console.WriteLine($"- Entity Path: {context.EntityPath}");
-            Console.WriteLine($"- Executing Action: {context.Action}");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;   
             return Task.CompletedTask;
         }
 
