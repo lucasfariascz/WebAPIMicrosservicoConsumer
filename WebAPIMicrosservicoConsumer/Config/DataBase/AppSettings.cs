8ff63c2859524b2603c361de1188732a954a5a8e
lucasfariascz <lucasfariasdacruz@gmail.com>
2023-11-01 15:04:32 -0300

    Remove Key

--- WebAPIMicrosservicoConsumer/Config/DataBase/AppSettings.cs
+++ WebAPIMicrosservicoConsumer/Config/DataBase/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSettings
     {
-        public static readonly string CosmosDdEndpointUri = "https://db2.documents.azure.com:443/";
-        public static readonly string CosmosDdPrimaryKey = "AUWmqnZgbARvvJsyC42sKBZ5kyiPGgHmuxCOnzHYUz3CC20iPMQ2xp8kjHQ0ieoyAP4vP8T1B6XDACDbtlRB6Q==";
+        public static readonly string CosmosDdEndpointUri = "";
+        public static readonly string CosmosDdPrimaryKey = "";
     }
 }
--- WebAPIMicrosservicoConsumer/Config/ServiceBus/AppSettings.cs
+++ WebAPIMicrosservicoConsumer/Config/ServiceBus/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public class AppSettings
     {
-        public static readonly string QueueName = "webapimicroqueue";
-        public static readonly string AzureServiceBus = "Endpoint=sb://webapimicroqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IDcSIe7o0vdw9qYOBf16LFRz03ST9Mobg+ASbPI3GOc=";
+        public static readonly string QueueName = "";
+        public static readonly string AzureServiceBus = "";
     }
 }
